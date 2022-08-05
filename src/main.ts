552dc3c366ebaa28ad67f729c427543fccdca3e3
Ugo Anayo <uggoprince@gmail.com>
2022-08-05 01:10:45 +0100

    fix bug on port variable

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,6 @@ async function bootstrap() {
   const config = new DocumentBuilder().setTitle('AutoMart 2.0').setDescription('The AutoMart API description').setVersion('2.0').build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  await app.listen(process.env.POR || 8080);
+  await app.listen(process.env.PORT || 8080);
 }
 bootstrap();
